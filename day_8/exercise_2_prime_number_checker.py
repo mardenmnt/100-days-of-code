002692753cc1b241b25cc0382e0e6d16bbeaf558
mardenmnt <marden.mnt@gmail.com>
2022-07-05 00:12:03 -0300

    Adicionando dia 8

---
+++ day_8/exercise_2_prime_number_checker.py
@@ -0,0 +1,56 @@
+
+"""
+Prime numbers are numbers that can only be cleanly divided by themselves and 1.
+
+https://en.wikipedia.org/wiki/Prime_number
+
+You need to write a function that checks whether if the number passed into it is a prime number or not.
+
+e.g. 2 is a prime number because it's only divisible by 1 and 2.
+
+But 4 is not a prime number because you can divide it by 1, 2 or 4.
+"""
+
+#Write your code below this line 👇
+
+def prime_checker(number):
+    is_prime = True
+    for num in range(2, number):
+        if (number % num) == 0:
+            is_prime = False
+
+    if is_prime:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")
+        
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number = n)
+
+
+
+## OR ##
+
+
+
+#Write your code below this line 👇
+
+def prime_checker(number):
+    rest = 0
+    for num in range(1, number + 1):
+        if (number % num) == 0:
+            rest += 1
+
+    if rest == 2:
+        print("It's a prime number.")
+    else:
+        print("It's not a prime number.")      
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number = n)
