055ff815398af477bee32f1c7ab2f5b94cc9ad75
mardenmnt <marden.mnt@gmail.com>
2022-07-07 00:43:28 -0300

    Atualizando dia 1

---
+++ day_01/Readme.md
@@ -0,0 +1,4 @@
+
+# Project 1 - Band Name Generator
+
+![Projeto Final](/day_1/project.gif)
---
+++ day_01/exercise_4_variables.py
@@ -0,0 +1,28 @@
+
+# Write a program that switches the values stored in the variables a and b
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+
+c = a
+a = b
+b = c
+
+
+# OR #
+# a,b = b,a
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
