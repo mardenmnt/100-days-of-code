82e3c5b54c636334ed5ac3946bb9401c333cabef
mardenmnt <marden.mnt@gmail.com>
2022-07-10 23:57:43 -0300

    Adicionando dia 13

---
+++ day_13/exercise_4_debugging_fizz_buzz.py
@@ -0,0 +1,10 @@
+
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:  ->  # 3 == 0 and number % 5
+#     print("FizzBuzz")
+#   if number % 3 == 0:   ->  # elif
+#     print("Fizz")
+#   if number % 5 == 0:   ->  # elif
+#     print("Buzz")
+#   else:
+#     print([number])     ->  # print(number)
